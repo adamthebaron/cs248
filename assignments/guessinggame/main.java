c722062881e202d862bdd064e6423b8080e596bd
Adam Kessler <akessle1@butler.edu>
2016-01-12 02:08:51 +0000

    add return variables and response()

--- assignments/guessinggame/main.java
+++ assignments/guessinggame/main.java
@@ -12,33 +12,37 @@ class main {
     "Might as well call you Deacon Blues... no? No one got that one?"
     };
 
-    /* receive/check input, then ask the user if they want to play again
+    private static int response(Scanner scanner) {
+        System.out.println("Would you like to play again?");
+        String answer = scanner.nextLine(); 
+        if (answer == "yes" || answer == "Yes" || answer == "y" || answer == "Y")
+            return 1;
+        else if (answer == "no" || answer == "No" || answer == "n" || answer == "N")
+            return 0;
+        return -1;
+    }
+
+    /* receive/check input, then ask the user if they want to play again and call response()
        flexible conditional statements make the program more user friendly */ 
     private static int guess(Scanner scanner) {
         System.out.println("Guess a number: ");
-        int number = (int) (1 + 100 * Math.random()); // cast as int due to Math.random() returning double
-        
+        int number = (int) (1 + 100 * Math.random()); // cast as int due to Math.random() returning double 
         for (int input = scanner.nextInt(); input != number; /* no increment */) {
             if (input < number)
                 System.out.println("Too low! " + insults[(int) (0 + 5 * Math.random())]);
             else if (input > number)
                 System.out.println("Too high! " + insults[(int) (0 + 5 * Math.random())]);
-        }
-        
-        System.out.println("Correct!\nWould you like to play again?");
-        String answer = scanner.nextLine();
-
-        if (answer == "yes" || answer == "Yes" || answer == "y" || answer == "Y")
-            guess(scanner);
-        else if (answer == "no" || answer == "No" || answer == "n" || answer == "N")
-            return 0;
-        else
-            System.out.println("I don't understand that answer.");
+        } 
+        System.out.println("Correct!\n");
+        if (response(scanner) == 1)
+                guess(scanner);
+        else if (response(scanner) == 0)
+                return 0;
         return -1;
     }
 
     /* main function; greet, init scanner, and call guess() */
-    private static int main() {
+    private static int main(String[] args) {
         System.out.println("Welcome!\n");
         Scanner scanner = new Scanner(System.in);
         if (guess(scanner) == 0)
