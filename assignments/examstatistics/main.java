30efca53f3b17cdcc70badc84036bf5299bbd9c5
Adam Kessler <akessle1@butler.edu>
2016-01-21 04:23:53 +0000

    format and add main loop

--- assignments/examstatistics/main.java
+++ assignments/examstatistics/main.java
@@ -2,37 +2,30 @@ import java.io.*;
 import java.util.*;
 
 class main {
-        FileReader fr = null;
-        FileWriter output = null;
-        Scanner input = null;
-        Grades grades = null;
+    FileReader fr = null;
+    FileWriter output = null;
+    Scanner input = null;
+    Grades grades = null;
 
-        public void main(String[] argv) 
-        throws IOException {
-            input = new Scanner(System.in);
-            System.out.print("Please enter grade data: ");
-            String filename = input.nextLine();
-            String nextline = null;
-            input.close();
+    public void main(String[] argv) 
+    throws IOException {
+        input = new Scanner(System.in);
+        System.out.print("Please enter grade data: ");
+        String filename = input.nextLine();
+        String nextline = null;
+        input.close();
+        do {
             try {
                 input = new Scanner(new FileReader(filename));
-                while ((nextline = input.readLine()) != null) {
-                    for (int i = 0; ;i++) {
-                        grades.num[i] = nextline;
-                    }
+                int i = 0;
+                while ((nextline = input.nextLine()) != null) {
+                    grades.num[i] = Integer.parseInt(nextline);
+                    i += 1;
                 }
             }
-        }
-
-
-
-
-        private void write_grades(String filename) 
-        throws IOException {
-            input = new Scanner(new FileReader(filename));
-            String curline;
-            while ((curline = input.readLine()) != null) {
-                
+            catch (FileNotFoundException ex) {
+                System.out.print("File not Found.\n");
             }
-        }
+        } while(true);
+    }
 }
