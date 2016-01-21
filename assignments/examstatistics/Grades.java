12e9e0329bf62fa66a33e954745f47be60b7606a
Adam Kessler <akessle1@butler.edu>
2016-01-21 04:10:29 +0000

    start main code, create Grades class

---
+++ assignments/examstatistics/Grades.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Grades {
+
+    int[] num;
+
+    int getMedian(String filename) {
+
+    }
+
+    int getAvg(String filename) {
+
+    }
+
+    int getMin(String filename) {
+
+    }
+
+    int getMax(String filename) {
+
+    }
+}
--- assignments/examstatistics/main.java
+++ assignments/examstatistics/main.java
@@ -2,5 +2,37 @@ import java.io.*;
 import java.util.*;
 
 class main {
+        FileReader fr = null;
+        FileWriter output = null;
+        Scanner input = null;
+        Grades grades = null;
 
+        public void main(String[] argv) 
+        throws IOException {
+            input = new Scanner(System.in);
+            System.out.print("Please enter grade data: ");
+            String filename = input.nextLine();
+            String nextline = null;
+            input.close();
+            try {
+                input = new Scanner(new FileReader(filename));
+                while ((nextline = input.readLine()) != null) {
+                    for (int i = 0; ;i++) {
+                        grades.num[i] = nextline;
+                    }
+                }
+            }
+        }
+
+
+
+
+        private void write_grades(String filename) 
+        throws IOException {
+            input = new Scanner(new FileReader(filename));
+            String curline;
+            while ((curline = input.readLine()) != null) {
+                
+            }
+        }
 }
