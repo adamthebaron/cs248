feea8ccbe1fd416fa2a59bb793e2a261ee152a96
Adam Kessler <akessle1@butler.edu>
2016-01-21 08:02:47 -0500

    add Grade class methods

--- assignments/examstatistics/Grades.java
+++ assignments/examstatistics/Grades.java
@@ -6,18 +6,25 @@ class Grades {
     public int[] num;
 
     public int getMedian(String filename) {
-
+		Array.sort(num);
+		int median = (num.length % 2);
+		return median;
     }
 
     public int getAvg(String filename) {
-
+		for (int i = 0; i <= num.length; i++) {
+			float total_score = num[i] + num[i + 1];
+			float average = total_score / num.length;
+		}
     }
 
     public int getMin(String filename) {
-
+		Array.sort(num);
+		return num[0];
     }
 
     public int getMax(String filename) {
-
+		Array.sort(num);
+		return num[num.length];
     }
 }
