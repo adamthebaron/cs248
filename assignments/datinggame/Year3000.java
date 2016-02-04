eb216874ad7135464b46860f34634b396a36996d
Adam Kessler <akessle1@butler.edu>
2016-02-03 21:09:42 -0500

    format code

--- assignments/datinggame/Year3000.java
+++ assignments/datinggame/Year3000.java
@@ -6,18 +6,17 @@ import java.io.*;
 */
 public class Year3000
 {
-  public static void main(String [] args) 
-  throws IOException
-  {
-    DateInterface d = new MyDate();
-    d.set(1, 25, 2015, 0);  // sets the date to Sunday, January 25th, 2015
-    while(d.getYear() < 3000)
+    public static void main(String[] args) 
+    throws IOException
     {
-      d.tomorrow();
-      // un-comment the next line to help with debugging
-      // System.out.println(d);
+        DateInterface d = new MyDate();
+        d.set(1, 25, 2015, 0);
+        while(d.getYear() < 3000)
+        {
+            d.tomorrow();
+            // System.out.println(d);
+        }
+        // at this point, d represents January 1, 3000
+        System.out.println(d);
     }
-    // at this point, d represents January 1, 3000
-    System.out.println(d);
-  }
 }
