487eaac81fc53012af1501bb44df8804b83d213a
Adam Kessler <akessle1@butler.edu>
2016-03-02 03:09:31 +0000

    rewrite data loading code

--- assignments/wumpus/Room.java
+++ assignments/wumpus/Room.java
@@ -4,6 +4,18 @@ import java.io.*;
 import java.util.*;
 
 class Room {
-	public int[] adjacentRooms = new int[3];
 	public String description;
+    public int roomNum;
+    public int adjroom1;
+    public int adjroom2;
+    public int adjroom3;
+
+    public Room(Scanner scanner) {
+        roomNum = scanner.nextInt();
+        adjroom1 = scanner.nextInt();
+        adjroom2 = scanner.nextInt();
+        adjroom3 = scanner.nextInt();
+        scanner.nextLine();
+        description = scanner.nextLine();
+    }
 }
