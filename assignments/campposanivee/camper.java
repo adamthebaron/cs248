78a527ca2e67b0ab8b0dfe1a4b79fea6a6554f74
Adam Kessler <akessle1@butler.edu>
2016-04-18 21:14:30 -0400

    added compareTo method

--- assignments/campposanivee/camper.java
+++ assignments/campposanivee/camper.java
@@ -1,5 +1,14 @@
-public class camper {
+public class camper implements Comparable {
     String name;
     int age;
     char sex;
+	
+	public int compareTo(Object obj) {
+		if (obj instanceof camper) {
+			camper c = (camper) obj;
+			return this.name.compareTo(c.name);
+		}
+		else
+			return -1;
+	}
 }
