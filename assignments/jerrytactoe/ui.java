be6d1974f2f8ca5609d7a205d291e2ad26d597ed
Adam Kessler <akessle1@butler.edu>
2016-04-25 08:49:53 -0400

    add input handling

--- assignments/jerrytactoe/ui.java
+++ assignments/jerrytactoe/ui.java
@@ -4,11 +4,24 @@ import java.awt.*;
 import javax.swing.*;
 
 public class ui extends JFrame {
+	public final int NONE;
+	public final int COMPUTER;
+	public final int USER;
+	
 	public static void main(String[] args) {
 		ui UI = new ui();
 	}
 	
 	public ui() {
-		
+		JButton[] spots = new JButton[9];
+		setTitle("Jerry Tac Toe";)
+		setSize(500,500);
+	}
+	
+	public bool changeText(JButton button, String text) {
+		if(button.getText().equals(null))
+			button.setText(text);
+		else
+			JOptionPane.showMessageDialog(ui,"Cannot select this space.");
 	}
 }
