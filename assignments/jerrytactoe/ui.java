19840cd4c4a0913dce3f80f5c74f19e2a6eb620e
Adam Kessler <akessle1@butler.edu>
2016-04-25 08:34:00 -0400

    start jerry tac toe

---
+++ assignments/jerrytactoe/ui.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class ui extends JFrame {
+	public static void main(String[] args) {
+		ui UI = new ui();
+	}
+	
+	public ui() {
+		
+	}
+}
